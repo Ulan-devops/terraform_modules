208531dacfcabaf9f9a65675d78641df19f62efe
farrukh90 <farrukhsadykov@gmail.com>
2019-08-03 16:28:38 -0500

    jom

--- module_instance.tf
+++ module_instance.tf
@@ -11,3 +11,16 @@ module "wordpress" {
   vpc_security_group_ids = ["sg-81a1d1ce"]
   subnet_id              = "subnet-4ff37d61"
 }
+module "joomla" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.6.0"
+
+  name           = "joomla"
+  instance_count = 3
+
+  ami                    = "ami-02eac2c0129f6376b"
+  instance_type          = "t2.micro"
+  key_name               = "macbook_farrukh"
+  vpc_security_group_ids = ["sg-81a1d1ce"]
+  subnet_id              = "subnet-4ff37d61"
+}

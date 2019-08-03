92faa2840d54b1ea05b3495e6e4651e313a2a436
farrukh90 <farrukhsadykov@gmail.com>
2019-08-03 17:00:43 -0500

    ingress_cidr_blocks      = ["10.10.0.0/16"]

--- sg.tf
+++ sg.tf
@@ -4,7 +4,8 @@ module "wordpress_sg" {
   name        = "wordpress"
   description = "some"
   vpc_id      = "vpc-1471ad6e"
-
+  
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
   ingress_rules            = ["https-443-tcp"]
   ingress_with_cidr_blocks = [
     {

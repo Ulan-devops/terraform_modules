b52b724dff002a719a82f2bbabbd207a6ec9cb35
farrukh90 <farrukhsadykov@gmail.com>
2019-08-03 16:12:06 -0500

    removed tags

--- module_instance.tf
+++ module_instance.tf
@@ -8,12 +8,6 @@ module "wordpress" {
   ami                    = "ami-0ad82a384c06c911e"
   instance_type          = "t2.micro"
   key_name               = "macbook_farrukh"
-  monitoring             = true
   vpc_security_group_ids = ["sg-81a1d1ce"]
   subnet_id              = "subnet-4ff37d61"
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
 }
